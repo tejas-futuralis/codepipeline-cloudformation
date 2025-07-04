c6176f8b8753ced7da72d0a8445c9077a0fc5fc4
Tejas Maloor <tejasmm2004@gmail.com>
2025-07-04 16:52:35 +0530

    Removed Parameters

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation deploy --template-file pipeline.yaml --stack-name MyPipelineStack --capabilities CAPABILITY_NAMED_IAM --parameter-overrides GitHubBranch=main GitHubConnectionArn=arn:aws:codeconnections:us-east-1:885885860822:connection/814e63dc-c551-415a-bb99-bf05ade9445f --profile AWSPowerUserAccess-885885860822
+aws cloudformation deploy --template-file pipeline.yaml --stack-name MyPipelineStack --capabilities CAPABILITY_NAMED_IAM --profile AWSPowerUserAccess-885885860822
