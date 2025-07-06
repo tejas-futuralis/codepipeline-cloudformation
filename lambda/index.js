0b3785b1423bc3bd41911812c5bcb1ce74c8352b
Tejas Maloor <tejasmm2004@gmail.com>
2025-07-07 01:36:06 +0530

    lambda 4

--- lambda/index.js
+++ lambda/index.js
@@ -1,7 +1,15 @@
-exports.handler = async (event) => {
-  console.log("Hello World!!! Lambda triggered from CodePipeline.");
+const AWS = require('aws-sdk');
+const codepipeline = new AWS.CodePipeline();
 
-  return {
-    status: "Success"
-  };
+exports.handler = async (event) => {
+  const jobId = event['CodePipeline.job']?.id;
+  if (!jobId) {
+    console.log('Not triggered by CodePipeline job, exiting...');
+    return;
+  }
+
+  // IMPORTANT: send success signal back to CodePipeline
+  await codepipeline.putJobSuccessResult({ jobId }).promise();
+
+  console.log('✅ CodePipeline job completed.');
 };
