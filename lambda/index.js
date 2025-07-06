6c0f6a199009c7a3f451a4c738f5d186dc5191dc
Tejas Maloor <tejasmm2004@gmail.com>
2025-07-06 23:33:10 +0530

    Chanded lambda code

--- lambda/index.js
+++ lambda/index.js
@@ -1,11 +1,93 @@
-const AWS = require('aws-sdk');
-const codepipeline = new AWS.CodePipeline();
-
-exports.handler = async (event) => {
-  const jobId = event["CodePipeline.job"]?.id;
-  if (jobId) {
-    await codepipeline.putJobSuccessResult({ jobId }).promise();
-  }
-  console.log("✅ Lambda invoked via CodePipeline.");
-  return;
+import { CodePipelineClient, PutJobSuccessResultCommand, PutJobFailureResultCommand } from "@aws-sdk/client-codepipeline";
+import http from 'http';
+import assert from 'assert';
+ 
+export const handler = (event, context) => {
+ 
+    const codepipeline = new CodePipelineClient();
+ 
+    // Retrieve the Job ID from the Lambda action
+    const jobId = event["CodePipeline.job"].id;
+ 
+    // Retrieve the value of UserParameters from the Lambda action configuration in CodePipeline, in this case a URL which will be
+    // health checked by this function.
+    const url = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters;
+ 
+    // Notify CodePipeline of a successful job
+    const putJobSuccess = async function(message) {
+        const command = new PutJobSuccessResultCommand({
+            jobId: jobId
+        });
+         try {
+             await codepipeline.send(command);
+             context.succeed(message);
+         } catch (err) {
+             context.fail(err); 
+         }
+    };
+ 
+    // Notify CodePipeline of a failed job
+    const putJobFailure = async function(message) {
+        const command = new PutJobFailureResultCommand({
+            jobId: jobId,
+            failureDetails: {
+                message: JSON.stringify(message),
+                type: 'JobFailed',
+                externalExecutionId: context.awsRequestId
+            }
+        });
+        await codepipeline.send(command);
+        context.fail(message);
+    };
+ 
+    // Validate the URL passed in UserParameters
+    if(!url || url.indexOf('http://') === -1) {
+        putJobFailure('The UserParameters field must contain a valid URL address to test, including http:// or https://');  
+        return;
+    }
+ 
+    // Helper function to make a HTTP GET request to the page.
+    // The helper will test the response and succeed or fail the job accordingly
+    const getPage = function(url, callback) {
+        var pageObject = {
+            body: '',
+            statusCode: 0,
+            contains: function(search) {
+                return this.body.indexOf(search) > -1;
+            }
+        };
+        http.get(url, function(response) {
+            pageObject.body = '';
+            pageObject.statusCode = response.statusCode;
+ 
+            response.on('data', function (chunk) {
+                pageObject.body += chunk;
+            });
+ 
+            response.on('end', function () {
+                callback(pageObject);
+            });
+ 
+            response.resume();
+        }).on('error', function(error) {
+            // Fail the job if our request failed
+            putJobFailure(error);
+        });
+    };
+ 
+    getPage(url, function(returnedPage) {
+        try {
+            // Check if the HTTP response has a 200 status
+            assert(returnedPage.statusCode === 200);
+            // Check if the page contains the text "Congratulations"
+            // You can change this to check for different text, or add other tests as required
+            assert(returnedPage.contains('Congratulations'));
+ 
+            // Succeed the job
+            putJobSuccess("Tests passed.");
+        } catch (ex) {
+            // If any of the assertions failed then fail the job
+            putJobFailure(ex);
+        }
+    });
 };
